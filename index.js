72cf1af14e245d30c00a4a68bc07a761c3a7d262
phuonghoangg <hoangphuong291199@gmail.com>
2023-04-04 21:28:15 +0700

    commit be

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const cors = require('cors')
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
-const cookiesParser = require('cookie-parser')
+const bodyParser = require("body-parser");
+
 const userRouter = require('./routes/userRouter')
 const typeProductRouter = require('./routes/typeProductRouter')
 const productRouter = require('./routes/productRouter')
@@ -14,11 +15,12 @@ mongoose.connect(process.env.MONGODB_URL,()=>{
    console.log("connected mongoose");
 })
 
-app.use(cors());
-app.use(cookiesParser());
-app.use(express.json());
-app.use(express.urlencoded());
+// app.use(cookiesParser());
+app.use(bodyParser.json({limit:"50mb"}))
 
+app.use(cors());
+// app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+// app.use(express.json());
 app.use('/v1/user',userRouter)
 app.use('/v2/type-product',typeProductRouter)
 app.use('/v3/product',productRouter)
